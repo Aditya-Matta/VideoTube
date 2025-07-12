83d1aea2790eb7409557a4462a48143e9849be65
Aditya Matta <princeaditya.1922@gmail.com>
2025-07-12 17:09:00 +0530

    add register controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -39,7 +39,7 @@ const userSchema = new Schema({
         type : String,
         required : [true, "Password is required"]
     },
-    refreshtoken: {
+    refreshToken: {
         type : String
     }
 }, {timestamps : true})
@@ -47,7 +47,7 @@ const userSchema = new Schema({
 userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
