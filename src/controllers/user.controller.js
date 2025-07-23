cc5100d015ae7a0aade48d8981e9bd9f4845c394
Aditya Matta <princeaditya@gmail.com>
2025-07-23 11:12:41 +0530

    correction in channel

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -351,7 +351,7 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
                 },
                 isSubscribed : {
                     $cond : {
-                        if: {$in: [req.users?._id, "$subscribers.subscriber"]},
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
                         then: true,
                         else: false
                     }
